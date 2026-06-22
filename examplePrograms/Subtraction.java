71e7887157a79fbcdcb4e5167223b6cf41ffcc04
sugan <sugankrishanan@gmail.com>
2026-06-22 22:12:19 +0530

    Sudtraction Program

---
+++ examplePrograms/Subtraction.java
@@ -0,0 +1,8 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int num1 = 10; 
+        int num2 = 5;
+        int difference = num1 - num2;
+        System.out.println("The difference of " + num1 + " and " + num2 + " is: " + difference);
+    }
+}
